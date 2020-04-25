4bc5cd777c89ee5fc24f3247885765dae634d2ca
kk77777 <kaushikgattani@gmail.com>
2020-04-25 18:53:35 +0530

    getting data from api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './App.css';
 
 export class App extends Component {
+  state = { advice: '' };
+
+  componentDidMount() {
+    this.fetchAdvice();
+  }
+
+  fetchAdvice = () => {
+    axios
+      .get('https://api.adviceslip.com/advice')
+      .then((res) => {
+        const { advice } = res.data.slip;
+        console.log(advice);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   render() {
     return (
       <div>
